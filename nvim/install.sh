e07933f3d7eddf50978445963fb59b48f9556b0f
blairfix <blairfix@gmail.com>
2023-12-16 21:45:52 -0700

    fixed color script

--- nvim/install.sh
+++ nvim/install.sh
@@ -2,8 +2,7 @@
 
 # make config directory
 mkdir -p ~/.config/nvim
-
 cp init.vim ~/.config/nvim/
 
-# vim plug
-curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+# theme
+./get_theme.sh
