a3eb9567d3a44cb8aa35e336a1cd662121c3267f
blairfix <blairfix@gmail.com>
2023-12-28 14:26:17 -0700

    added dynamic font size + old yml

--- alacritty/install.sh
+++ alacritty/install.sh
@@ -4,6 +4,7 @@ dir="/home/blair/.config/alacritty/"
 
 mkdir -p  $dir
 cp alacritty.toml $dir
+cp alacritty.yml $dir
 
 # get current host
 host=$(hostname)
@@ -12,8 +13,10 @@ host=$(hostname)
 if [ $host == 'blair-server' ]
 then
    sed -i 's/font-size-here/11/g' "$dir"/alacritty.toml
+   sed -i 's/font-size-here/11/g' "$dir"/alacritty.yml
 
 else
     sed -i 's/font-size-here/13/g' "$dir"/alacritty.toml
+   sed -i 's/font-size-here/11/g' "$dir"/alacritty.yml
 fi
 
