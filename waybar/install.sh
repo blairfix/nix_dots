26d1da761b72cc96d0a9881fd0bb38a7dd583963
blairfix <blairfix@gmail.com>
2024-01-02 22:11:17 -0700

    dynamic font

--- waybar/install.sh
+++ waybar/install.sh
@@ -6,3 +6,14 @@ mkdir -p  $config
 cp config $config
 cp style.css $config
 
+# get current host
+host=$(hostname)
+
+# set font size
+if [ $host == 'blair-server' ]
+then
+   sed -i 's/font-size-here/16/g' "$config"/style.css
+else
+    sed -i 's/font-size-here/18/g' "$dir"/style.css
+fi
+
